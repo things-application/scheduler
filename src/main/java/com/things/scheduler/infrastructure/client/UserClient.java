cb625f2922aec67c8a632553b47d46a8fe939b17
Maxson Almeida <maxsonferovante@gmail.com>
2025-03-18 23:34:18 -0300

    add config security and user client feign

---
+++ src/main/java/com/things/scheduler/infrastructure/client/UserClient.java
@@ -0,0 +1,16 @@
+package com.things.scheduler.infrastructure.client;
+
+
+import com.things.scheduler.business.dto.UserResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "users", url="${url.service.users}")
+public interface UserClient {
+
+    @GetMapping
+    UserResponse getUserByEmail(@RequestHeader("Authorization") String token,
+                                @RequestParam String email);
+}
